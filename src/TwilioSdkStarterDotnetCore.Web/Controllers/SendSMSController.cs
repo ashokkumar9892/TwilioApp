7cbc15ae135eda322ce598aa23879d3f57545329
Ludega <customer.support@ludega.com>
2022-04-18 19:35:17 -0400

    Update Appsettings

--- src/TwilioSdkStarterDotnetCore.Web/Controllers/SendSMSController.cs
+++ src/TwilioSdkStarterDotnetCore.Web/Controllers/SendSMSController.cs
@@ -11,7 +11,7 @@ namespace TwilioSdkStarterDotnetCore.Web.Controllers
     public class SendSMSController : Controller
     {
         private readonly TwilioAccount _twilioAccount;
-        private readonly string authToken = "2010e55534c3ad56daee55b300fb1033";
+        private readonly string authToken = "0691c867a7be4d8713a45114243c2d5a";
 
         public SendSMSController(IOptions<TwilioAccount> twilioAccount)
         {
@@ -29,7 +29,7 @@ namespace TwilioSdkStarterDotnetCore.Web.Controllers
 
             var message = MessageResource.Create(
                 body: text,
-                from: new Twilio.Types.PhoneNumber("+19592071545"),
+                from: new Twilio.Types.PhoneNumber("+12764008669"),
                 to: new Twilio.Types.PhoneNumber("+1" + mobnumber)
             );
             return "Message Sent to Mobile : " + mobnumber;
@@ -41,7 +41,7 @@ namespace TwilioSdkStarterDotnetCore.Web.Controllers
             TwilioClient.Init(_twilioAccount.AccountSid, authToken);
 
             var messageOptions = new CreateMessageOptions(
-                new PhoneNumber("whatsapp:+919458412124"));
+                new PhoneNumber("whatsapp:+17186979892"));
             messageOptions.From = new PhoneNumber("whatsapp:+14155238886");
             messageOptions.Body = "Your appointment sample msg";
 
